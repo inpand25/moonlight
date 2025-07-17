9f82336bd5cdda3ce57315ba88a159587edd1c28
Mr-Yozz <yogeshwaran03042002@gmail.com>
2025-07-17 15:49:18 +0530

    filesetup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn moonlightenterprises_project.wsgi
--- moonlightenterprises_project/settings.py
+++ moonlightenterprises_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR/'static'
